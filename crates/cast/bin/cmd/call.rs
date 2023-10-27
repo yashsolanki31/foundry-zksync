4c0e1cecd664b5dd22deb758bdda6137aab5a2ee
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-10-27 08:14:05 -0500

    chore: updates docs, readme, templates, workflow updates (#130)

--- crates/cast/bin/cmd/call.rs
+++ crates/cast/bin/cmd/call.rs
@@ -217,6 +217,7 @@ impl CallArgs {
 }
 
 /// fills the builder from create arg
+#[allow(clippy::needless_pass_by_ref_mut)]
 async fn fill_create(
     builder: &mut TxBuilder<'_, Provider>,
     value: Option<U256>,
@@ -239,6 +240,7 @@ async fn fill_create(
 }
 
 /// fills the builder from args
+#[allow(clippy::needless_pass_by_ref_mut)]
 async fn fill_tx(
     builder: &mut TxBuilder<'_, Provider>,
     value: Option<U256>,
